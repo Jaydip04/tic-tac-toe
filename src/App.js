76cfc93bae3f6c47b1dbc8f707af95de6f384a5c
Jaydip Tank <95556824+Jaydip04@users.noreply.github.com>
2023-01-28 14:11:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from 'react'
-import Header from './Components/Header/Header'
+import React, { useEffect, useState } from "react";
+import Header from "./Components/Header/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-import ScoreBoard from './Components/ScoreBoard/ScoreBoard';
+import ScoreBoard from "./Components/ScoreBoard/ScoreBoard";
 
 const App = () => {
   const [activePlayerName, setActivePlayerName] = useState("");
   const [activePlayer, setActivePlayer] = useState(1);
   const [scoreO, setScoreO] = useState(0);
   const [scoreX, setScoreX] = useState(0);
-  const [gameActive, setGameActive] = useState(false);
+  const [gameActive, setGameActive] = useState(true);
 
   const arr = Array.from({ length: 9 }, (v, i) => i + 1);
 
@@ -23,8 +23,7 @@ const App = () => {
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
-  ]
-
+  ];
 
   const handelClick = (id) => {
     console.log(id);
@@ -32,18 +31,67 @@ const App = () => {
     if (!gameActive) {
       gameRestart();
     }
+    if (gameState[id] === 2) {
+      gameState[id] = activePlayer;
+      if (activePlayer === 0) {
+        setActivePlayer(1);
+        btn.innerText = "O";
+      }
+      if (activePlayer === 1) {
+        setActivePlayer(0);
+        btn.innerText = "X";
+      }
+    }
+
+    // check win
+    winPositions.forEach((element) => {
+      if (
+        gameState[winPositions[0]] === gameState[winPositions[1]] &&
+        gameState[winPositions[1]] === gameState[winPositions[2]] &&
+        gameState[winPositions[0]] !== 2
+      ) {
+        let WinStr = "";
+        setGameActive(false);
+        if (gameState[winPositions[0]] === 0) {
+          WinStr = "X has won";
+          setScoreX(scoreX + 1);
+        } else {
+          WinStr = "O has won";
+          setScoreO(scoreO + 1);
+        }
+        alert(WinStr);
+      }
+    });
+    // check game is end or not
+    let isGameEnd = true;
+    for (let index = 0; index < gameState.length; index++) {
+      if (gameState[index] === 2) {
+        isGameEnd = false;
+      }
+    }
+    if (isGameEnd && gameActive === true) {
+      setGameActive(false);
+      isGameEnd = true;
+      activePlayerName("Game is Over.\nStart new Game.");
+    }
   };
 
   useEffect(() => {
     if (activePlayer === 1) {
-      setActivePlayerName("X's");
-    }
-    else {
-      setActivePlayerName("O's");
+      setActivePlayerName("X's - Tap to play");
+    } else {
+      setActivePlayerName("O's - Tap to play");
     }
   }, [activePlayer]);
 
-  const gameRestart = () => { };
+  const gameRestart = () => {
+    arr.map((item, index) => {
+      let btn = document.getElementById(`btn-${index}`);
+      btn.innerText = "";
+      return null;
+    });
+    setActivePlayer(1);
+  };
 
   return (
     <>
@@ -56,18 +104,28 @@ const App = () => {
         <div className="d-flex align-content-center justify-content-center mt-3">
           <div className="game-board">
             {arr.map((item, index) => {
-              return <button key={index} className='blank-btn' id={`btn-${item}`} onClick={() => handelClick(item)}>{ }</button>
+              return (
+                <button
+                  key={index}
+                  className="blank-btn"
+                  id={`btn-${index}`}
+                  onClick={() => handelClick(index)}
+                ></button>
+              );
             })}
-
           </div>
         </div>
         <div className="footer">
-          <p id="curentplayer"><span id='player-status'>{activePlayerName}</span> turn</p>
-          <button className='reset-btn' onClick={gameRestart}>Reset</button>
+          <p id="curentplayer">
+            <span id="player-status">{activePlayerName}</span> turn
+          </p>
+          <button className="reset-btn" onClick={gameRestart}>
+            Reset
+          </button>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
